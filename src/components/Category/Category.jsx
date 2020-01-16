9025b52683142a276eb551a146e820f3d9076c10
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-01-16 15:23:11 -0600

    created nav and header files

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Category(props) {
+    return (
+      <div className="component">
+       
+          <div>
+            <p>Category</p>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Category;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import TypoGraphy from "@material-ui/core/Typography";
+
+
+function Nav(props) {
+    return (
+      <List component="nav">
+        <ListItem component="div">
+          <ListItemText inset>
+            <TypoGraphy color="inherit" variant="subtitle1">
+              Home
+            </TypoGraphy>
+          </ListItemText>
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="subtitle1">
+            Add Recipe
+          </TypoGraphy>
+        </ListItemText>
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="subtitle1">
+            View Recipe
+          </TypoGraphy>
+        </ListItemText>
+      </ListItem>
+    </List>
+  );
+}
+
+  
+  export default Nav;
